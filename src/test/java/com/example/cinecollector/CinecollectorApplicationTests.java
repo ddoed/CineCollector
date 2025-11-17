0b1337e4a606d53f5bddba5a4059afd815fec117
s202455374 <yimnabin20051022@gmail.com>
2025-11-17 12:55:15 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/com/example/cinecollector/CinecollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinecollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinecollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
